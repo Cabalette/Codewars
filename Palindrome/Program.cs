27f16c160fcd08e0a4aafe635ae43601a499387d
Dmitriy <123815654+Cabalette@users.noreply.github.com>
2023-03-14 16:56:46 +0300

    Добавьте файлы проекта.

---
+++ Palindrome/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Numerics;
+using System;
+using System.Reflection;
+
+namespace Palindrome
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(WordSpinner("aaab bc ccccccd"));
+            int a = 3;
+        }
+
+        static string WordSpinner(string sentence)
+        {
+            return String.Join(" ", (sentence.Split(' ').
+                         Select(x =>
+                         {
+                             if (x.Length > 4)
+                             {
+                                 char[] sReverse = x.ToCharArray();
+                                 Array.Reverse(sReverse);
+                                 x = new string(sReverse);
+                             }
+                             return x;
+                         }).                         
+                         ToArray()));
+        }
+    }
+    }
