28b66d417e2886b5add7d504f95c02556c3d9fc2
Dmitriy <s.d98@mail.ru>
2023-03-14 22:02:26 +0300

    Задачи про лайки

---
+++ Who likes it/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Who_likes_it
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] example = new string[5] { "Peter", "Alex", "Dima", "Vera", " " };
+            Console.WriteLine(Kata.Likes(example));
+
+        }
+        public static class Kata
+        {
+            public static string Likes(string[] name)
+            {
+                if (name.Length == 0) return "no one likes this";
+                else if (name.Length == 1) return $"{name[0]} likes this";
+                else if (name.Length == 2) return $"{name[0]} and {name[1]} like this";
+                else if (name.Length == 3) return $"{name[0]}, {name[1]} and {name[2]} like this";
+                else return $"{name[0]}, {name[1]} and {name.Length - 2} others like this";
+            }
+        }
+    }
+}
