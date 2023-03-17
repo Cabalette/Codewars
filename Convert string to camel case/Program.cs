618193af4d5a49450042b87cda668e3cbf7f02f3
Dmitriy <123815654+Cabalette@users.noreply.github.com>
2023-03-17 08:30:58 +0300

    Новая задача

---
+++ Convert string to camel case/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Convert_string_to_camel_case
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Kata.ToCamelCase("the-stealth-warrior"));
+        }
+        public class Kata
+        {
+            public static string ToCamelCase(string str)
+            {
+                return string.Join("", str.Split('_', '-').
+                                           Select((x, idx) => idx != 0 ? x[0].ToString().ToUpper() + x.Substring(1) : x));
+            }
+        }
+    }
+}
